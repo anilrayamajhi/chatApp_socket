eec6eece83e877fd0575f4d7511a1316b24526d3
Anil Rayamajhi <anilrm07@gmail.com>
2016-10-06 11:45:47 -0700

    chat function working

---
+++ server.js
@@ -0,0 +1,35 @@
+var
+  app = require('express')(),
+  http = require('http').Server(app),
+  socketIO = require('socket.io'),
+  socketServer = socketIO(http);
+
+var port = (process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+  // res.sendfile('index.html'); inthis case same as
+  res.sendFile('/index.html', {root: __dirname});
+
+});
+
+socketServer.on('connection', function(socket){
+  console.log('A client connected');
+
+    socket.on('mexico', function(phrase){
+        console.log(phrase);
+        socket.emit('mex-res', 'Igot your message');
+    })
+
+    socket.on('chat message', function(phrase){
+      console.log(phrase);
+    })
+
+
+    socket.on('disconnect', function(socekt){
+      console.log('A client is disconnected');
+    });
+})
+
+http.listen(port, function(){
+  console.log(`Server start end on port: ${port}`);
+});
